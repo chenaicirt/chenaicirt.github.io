9bbf3051811c24be6cd7fd844067f1470b4a4a87
chenaicirt <chenaicirt@gmail.com>
2017-12-05 10:38:25 -0500

    adding regular expressions

--- js/webspeech.js
+++ js/webspeech.js
@@ -84,6 +84,53 @@ $(document).on('keydown', function ( e ) {
          return s.replace(first_char, function(m) { return m.toUpperCase(); });
         }
 
+  var potentialcommand = $("#command").val();
+
+  console.log(potentialcommand);
+
+    var re = /^(click|scroll|enter)\s(.*)/i;
+    var result = re.exec(potentialcommand);
+
+    if(result) {
+    var verb = result[1];
+      var arg = result[2]
+
+      console.log("verb: " + verb + ", args: " + arg)
+
+      switch(verb) {
+        case "click":
+          // what do we do if click?
+          console.log("handling click")
+
+          //console.log($("*:contains('" + arg + "')"));
+
+          var stringpieces = arg.split(/\s/);
+
+          $("a,input,button").each(function() {
+            if($(this)[0].tagName == "INPUT") {
+              // look for the text in INPUT fields
+            } else if() {
+
+            }
+          })
+
+          break;
+        case "scroll":
+          // what do we do on scroll?
+          console.log("handling scroll`")
+          break;
+        case "enter":
+          // what do we do on enter?
+          console.log("handling enter")
+          break;
+        default:
+          console.log("sorry, that is not a recognized command")
+      }
+
+
+
+
+
 
     
 
